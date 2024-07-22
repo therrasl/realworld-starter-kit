c20081c444edb9d58cfabc07dd4eca5b0f84fcd9
therrasl <164066143+therrasl@users.noreply.github.com>
2024-07-22 20:23:45 +0300

    Update articles.service.ts

--- src/app/user/articles/articles.service.ts
+++ src/app/user/articles/articles.service.ts
@@ -1,13 +1,16 @@
-import { HttpClient } from "@angular/common/http";
-import { inject, Injectable } from "@angular/core";
-import { Observable } from "rxjs";
-import { Articles } from "./model/articles";
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Articles } from './model/articles';
 
-@Injectable({providedIn: 'root'})
+@Injectable({ providedIn: 'root' })
 export class ArticlesService {
-    private readonly http = inject(HttpClient);
+  
+  private readonly http = inject(HttpClient);
 
-    getArticles(): Observable<Articles[]> {
-        return this.http.get<Articles[]>('https://api.realworld.io/api/articles?limit=10&offset=0')
-    }
+  getArticles(): Observable<Articles[]> {
+    return this.http.get<Articles[]>(
+      'https://api.realworld.io/api/articles?limit=10&offset=0'
+    );
+  }
 }
